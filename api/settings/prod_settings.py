7aba1f9082a8b4854f90a3b43cb9178678072caa
sagar sangwan <iamsagarsangwan@gmail.com>
2024-05-01 01:41:55 +0530

    ?

--- api/settings/prod_settings.py
+++ api/settings/prod_settings.py
@@ -18,19 +18,19 @@ STATICFILES_DIRS = [
 # Configures the staticfiles directory to serve
 # static files from /static/ on our deployment
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "HOST": os.environ.get("HOST"),
-#         "NAME": os.environ.get("NAME"),
-#         "USER": os.environ.get("USER"),
-#         "PASSWORD": os.environ.get("PASSWORD"),
-#         "PORT": os.environ.get("PORT"),
-#     }
-# }
-import dj_database_url
-
-SUPABASE_DB_URL = os.environ.get("SUPABASE_DB_URL")
 DATABASES = {
-    "default": dj_database_url.config(default=SUPABASE_DB_URL, conn_max_age=600)
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("HOST"),
+        "NAME": os.environ.get("NAME"),
+        "USER": os.environ.get("USER"),
+        "PASSWORD": os.environ.get("PASSWORD"),
+        "PORT": os.environ.get("PORT"),
+    }
 }
+# import dj_database_url
+
+# SUPABASE_DB_URL = os.environ.get("SUPABASE_DB_URL")
+# DATABASES = {
+#     "default": dj_database_url.config(default=SUPABASE_DB_URL, conn_max_age=600)
+# }
