f510d9da0c278e08a43bd618c0f2f39098de911b
sagar sangwan <iamsagarsangwan@gmail.com>
2024-05-01 00:40:35 +0530

    added buils.sh files

--- api/settings/prod_settings.py
+++ api/settings/prod_settings.py
@@ -1,8 +1,13 @@
+import dj_database_url
+import os
+
+
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", "now.sh"]
 
 STATIC_URL = "static/"
-
-
-DATABASES = {}
+SUPABASE_DB_URL = os.environ.get("SUPABASE_DB_URL")
+DATABASES = {
+    "default": dj_database_url.config(default=SUPABASE_DB_URL, conn_max_age=600)
+}
