a99f511d52944b51efa71e6f1a80f7c534efd830
sagar sangwan <iamsagarsangwan@gmail.com>
2024-05-01 17:48:26 +0530

    for render

--- api/settings/prod_settings.py
+++ api/settings/prod_settings.py
@@ -4,7 +4,7 @@ from api.settings.base_settings import BASE_DIR
 
 
 DEBUG = True
-ALLOWED_HOSTS = [".vercel.app", "semantic-chat-api.vercel.app", ".now.sh"]
+ALLOWED_HOSTS = [".vercel.app", "semantic-chat-api.vercel.app", ".now.sh", "127.0.0.1"]
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 #
