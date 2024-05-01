d0e2705c666ed88892cdb52a28e0dade23ee2a1f
sagar sangwan <iamsagarsangwan@gmail.com>
2024-05-01 12:06:47 +0530

    ???

--- core/models.py
+++ core/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+
+class Personsss(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
