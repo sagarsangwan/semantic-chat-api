0ba555d6cf60cbe6de156609e4faa0b6bc46dd04
sagar sangwan <iamsagarsangwan@gmail.com>
2024-05-01 10:52:16 +0530

    updated build file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
-pip install -r requirements.txt
-python3.9 manage.py migrate --no-input
+# Build the project
+echo "Building the project..."
+python3.10 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
