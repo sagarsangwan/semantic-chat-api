47330e8263a583ebad143616e393b2366a817ada
sagar sangwan <iamsagarsangwan@gmail.com>
2024-05-15 19:01:03 +0530

    ????

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ def index(request):
     html = f"""
     <html>
         <body>
-            <h1>Hello from core app</h1>
+            <h1>Hello from core app sagar</h1>
             <p>The current time is { now }. </p>
         </body>
     </html>
